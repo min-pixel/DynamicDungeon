64e42eb333bf56f342602d213cf8c619894fc8b5
min-pixel <toalsgur@naver.com>
2025-02-24 21:28:16 +0900

    경로에 맞게 스폰되는 복도와 기존 복도가 연결되지 않는 문제 해결.
    경로 탐색 시 목표 지점의 타일을 주변 인접 타일을 고려하여, 연결되는 방향의 벽이 동적으로 삭제되는 특수 타일로 교체되도록 수정하여 해결..

--- Source/project03/Public/WaveFunctionCollapseSubsystem02.h
+++ Source/project03/Public/WaveFunctionCollapseSubsystem02.h
@@ -88,8 +88,8 @@ public:
 	TArray<int32>GetAllAdjacentIndices(int32 TileIndex, FIntVector GridResolution);
 
 	TArray<int32>GetTwoStepAdjacentIndices(int32 TileIndex, FIntVector GridResolution);
-
-	TArray<int32> GetAllAdjacentIndices02(int32 TileIndex, FIntVector GridResolution);
+	void ReplaceGoalTileWithCustomTile(
+		int32 GoalTileIndex, TArray<FWaveFunctionCollapseTileCustom>& Tiles);
 	
 	/**
 	* Solve a grid using a WFC model.  If successful, spawn an actor.
